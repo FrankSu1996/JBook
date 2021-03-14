44d7a1276b807eba1347faa964a76bf0bb5472c4
Frank Su <franksu49@gmail.com>
2021-03-14 15:26:29 -0400

    implemented bundling of tiny-test-pkg from unpkg

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -1,14 +1,27 @@
 import * as esbuild from "esbuild-wasm";
+import axios from "axios";
 
 export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
+      // need custum implementation of onResolve, which is called when esBuild trying to
+      // locate file from import statement. We need to redefine path to fetch from unpkg.com
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResole", args);
-        return { path: args.path, namespace: "a" };
+        console.log("onResolve", args);
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        } else if (args.path === "tiny-test-pkg") {
+          return {
+            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
+            namespace: "a",
+          };
+        }
       });
 
+      // need custom implementation of onLoad, which is called when esBuild trying to
+      // load the file from import system. Default is loading from file system, we need to
+      // fetch contents from unpkg using path defined in build.onResolve
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log("onLoad", args);
 
@@ -16,16 +29,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from './message';
+              const message = require('tiny-test-pkg')
               console.log(message);
             `,
           };
-        } else {
-          return {
-            loader: "jsx",
-            contents: 'export default "hi there!"',
-          };
         }
+
+        const { data } = await axios.get(args.path);
+        return {
+          loader: "jsx",
+          contents: data,
+        };
       });
     },
   };
